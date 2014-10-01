352730bc1843068d4c288fcec8ec4188c5c4d56b
alexantosh <alexantosh@yahoo.com>
2014-10-01 23:42:37 +0300

    Testing Multiple Projects With a Shared Repository

---
+++ com.egit.lex.java1/src/com/egit/lex/java1/Mutli2.java
@@ -0,0 +1,11 @@
+package com.egit.lex.java1;
+
+public class Mutli2
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello mutliple git 2");
+	}
+
+}
---
+++ com.egit.lex.java2/src/com/egit/lex/java2/Mutli1.java
@@ -0,0 +1,11 @@
+package com.egit.lex.java2;
+
+public class Mutli1
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello mutliple git 1");
+	}
+
+}
